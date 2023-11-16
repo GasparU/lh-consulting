1869b073d5987fc52c809c8c853a9ac63c1200c0
Jose Gaspar <sainc2016@gmail.com>
2023-11-15 19:08:00 -0500

    cambiando imagenes

--- src/components/ImagenHome.jsx
+++ src/components/ImagenHome.jsx
@@ -10,29 +10,29 @@ function ImagenHome() {
 
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 1;
-    const newIndex = isFirstSlide ? 3 - 1 : currentIndex - 1;
+    const newIndex = isFirstSlide ? 3 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
 
   const nextSlide = () => {
-    const isLastSlide = currentIndex === 3 - 1;
+    const isLastSlide = currentIndex === 3;
     const newIndex = isLastSlide ? 1 : currentIndex + 1;
     setCurrentIndex(newIndex);
   };
 
   return (
-    <div className='  w-full m-auto  relative group  '>
-      <span className='static  '>
-        <Image src={`/slider${currentIndex}.jpg`} width={1400} height={930} className='w-full h-full  bg-center bg-cover ' alt="imagen" />
+    <div className=' w-full m-auto overflow-hidden  relative group  '>
+      <span className='static'>
+        <Image src={`/slider${currentIndex}.jpg`} width={1400} height={40} 
+        className='w-full xl:h-[80vh]' alt="imagen" />
         <ImagenHomeIcons />
       </span>
-
-      {/* Left Arrow */}
-      <span className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+      <span className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl 
+      rounded-full p-2 bg-black/20 text-white cursor-pointer'>
         <BsChevronCompactLeft onClick={prevSlide} size={30} />
       </span>
-      {/* Right Arrow */}
-      <span className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+      <span className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl 
+      rounded-full p-2 bg-black/20 text-white cursor-pointer'>
         <BsChevronCompactRight onClick={nextSlide} size={30} />
       </span>
     </div>
