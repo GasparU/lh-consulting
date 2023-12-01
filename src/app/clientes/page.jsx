d0ab9c06e5918ccf265a593aed7b55a78f8db1ef
Jose Gaspar <sainc2016@gmail.com>
2023-11-30 23:39:36 -0500

    manejando clientes

--- src/app/clientes/page.jsx
+++ src/app/clientes/page.jsx
@@ -1,28 +1,59 @@
-import Image from 'next/image'
+'use client'
 import React from 'react'
+import { motion } from "framer-motion";
 
-const logos = [ 
-    {empresas: "/LOGO_CAJABAMBA.png"}, {empresas: "/LOGO_LEDESMA.jpeg"}, {empresas: "/LOGO_PARDO.png"}, {empresas: "/LOGO_MERCEDES.png"},
-    {empresas: "/LOGO_CUART.png"}, {empresas: "/LOGO_DYNO.png"}, {empresas: "/LOGO_EMS.png"}, {empresas: "/LOGO_GOVI.png"},
-    {empresas: "/LOGO_LOGISTIC.png"}, {empresas: "/LOGO_PANAMEDICAL.png"}, {empresas: "/LOGO_AGM.jpg"},
+const quote = {
+    initial: {
+        opacity: 1,
+    },
+    animate: {
+        opacity: 1,
+        transition: {
+            delay: 0.02,
+            staggerChildren: 0.08
+        }
+    }
+}
+const singelWord = {
+    initial: {
+        opacity: 0,
+        y: 50,
+    },
+    animate: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 1
+        }
+    }
+}
+
+const logos = [
+    { empresas: "/LOGO_CAJABAMBA.png" }, { empresas: "/LOGO_LEDESMA.jpeg" }, { empresas: "/LOGO_PARDO.png" }, { empresas: "/LOGO_MERCEDES.png" },
+    { empresas: "/LOGO_CUART.png" }, { empresas: "/LOGO_DYNO.png" }, { empresas: "/LOGO_EMS.png" }, { empresas: "/LOGO_GOVI.png" },
+    { empresas: "/LOGO_LOGISTIC.png" }, { empresas: "/LOGO_PANAMEDICAL.png" }, { empresas: "/LOGO_AGM.jpg" },
 ]
 
-const ImageClient = ({src}) => {
-    return (
-        <div className='flex w-52 h-52 bg-white justify-center items-center rounded-xl hover:opacity-75 bg-indigo duration-200
-        hover:scale-110'>
-            <Image className='saturate-800 w-max h-max' src={src} width={350} height={150} alt='whatsapp' />
-        </div>
-
-    )
-}
 
 export default function ClientPage() {
     return (
-        <div className='flex gap-8 flex-wrap m-10 justify-items-center content-center justify-center'>
-           {
-            logos.map(logo => (<ImageClient key={logo.empresas} src={logo.empresas}/>))
-           }
+
+        <div className='py-2'>
+            <motion.div className={`flex items-center flex-wrap gap-10 m-10 `}
+                variants={quote}
+                initial="initial"
+                animate="animate"
+            >
+                {
+                    logos.map((logo) =>
+                        <motion.img key={logo.empresas} className=' w-96 h-44 rounded-xl object-scale-down bg-white hover:scale-75 duration-200' src={logo.empresas}
+                            variants={singelWord}
+                        >
+
+                        </motion.img>)
+                }
+            </motion.div>
         </div>
+
     )
 }
